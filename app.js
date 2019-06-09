e2222ad18c87f2c425f0b497a6e54bac08865884
Nate Beers <beers@Nates-MBP.fios-router.home>
2019-06-09 12:37:49 -0400

    Added authToken check and name/type query params

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
 const morgan = require('morgan');
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const app = express();
 app.use(morgan('dev'));
 app.use(helmet());
+app.use(cors());
 app.use(express.json());
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,8 +1,55 @@
 const app = require('./app.js');
+const POKEDEX = require('./pokedex.json');
 
-app.get('/', (req, res) => {
-  res.send("Hello!!!")
+app.use(function validateBearerToken(req,res,next){
+  const bearerToken = req.get('Authorization');
+  const apiToken = process.env.API_TOKEN;
+
+  if(!bearerToken){
+    return res.status(401).json({
+      error: 'Unauthorized request: Token must be passed as Authorization header'
+    });
+  }
+
+  if(bearerToken.split("  ")[1] !== apiToken){
+    return res.status(401).json({
+      error: 'Unauthorized request: bad token'
+    });
+  }
+  
+  next();
 });
+
+function handleGetTypes(req,res){
+  const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
+  res.json(validTypes);
+}
+
+app.get('/types', handleGetTypes);
+
+function handleGetPokemon(req,res){
+  const { name, type } = req.query;
+
+  if(name && type){
+    console.log(name,type)
+    const pokemonsNameType = POKEDEX.pokemon.filter( poke => (
+      poke.name.toLowerCase().includes(name.toLowerCase()) && poke.type.includes(type)
+    ));
+    return res.json(pokemonsNameType)
+  }else if(name){
+    const pokemonsByName = POKEDEX.pokemon.filter( poke => (
+      poke.name.toLowerCase().includes(name.toLowerCase())
+    ));
+    return res.json(pokemonsByName);
+  }else if(type){
+    const pokemonsByType = POKEDEX.pokemon.filter( poke => (
+      poke.type.includes(type)
+    ));
+    return res.json(pokemonsByType);
+  }
+}
+
+app.get('/pokemon', handleGetPokemon);
 
 app.listen(4000, () => {
   console.log("Chillin' on port 4k...")
